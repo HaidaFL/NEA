dad2a6fd62324ca7518d7600c7877d4c40a7d30f
HaidaFL <152400682+HaidaFL@users.noreply.github.com>
2023-12-11 16:02:35 +0000

    modelling started(opentk)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 
 //TARGETS: 1)redo co-ordinate system and make it out of floats, doesnt work otherwise 2) then put the youngs modulus bit in there w forces properly
@@ -7,12 +9,12 @@ using System.Collections.Generic;
 
 public class AddCoordinates
 {
-    public static void AddPositiveCoordinates(List<Tuple<int, int, int>> list, int x, int y, int z)
+    public static void AddPositiveCoordinates(List<(double x, double y, double z)> list, double x, double y, double z)
     {
 
         if (x >= 0 && y >= 0 && z >= 0)
         {
-            list.Add(new Tuple<int, int, int>(x, y, z));
+            list.Add(( x, y, z));
         }
         else
         {
@@ -20,21 +22,33 @@ public class AddCoordinates
         }
     }
 }
+public class Force
+{
+    public float g = 9.81f;
+    public Tuple<double, double, double> XYZforce { get; set; }
 
+    public Force(Tuple<double, double, double> xyzforce)
+    { XYZforce = xyzforce; }
+    public double ReactionForce(float mass, float XYZforce)
+    {
+        return ((mass * g) + XYZforce);
+    }
+}
 public class PhysicsObject
 {
     // Properties
-    public float Mass { get; private set; }
-    public float Velocity { get; private set; }
-    public float YoungsModulus { get; private set; }
-    public float Deformation { get; private set; }
-    public float MaxDeformation { get; private set; }
-    public int GroundLevel { get; private set; }
-    public float Area { get; private set; }
-    public float ForceToStayAboveGround { get; private set; }
+    public float Mass { get; protected set; }
+    public float Velocity { get; protected set; }
+    public float YoungsModulus { get; protected set; }
+    public float Deformation { get; protected set; }
+    public float MaxDeformation { get; protected set; }
+    public double GroundLevel { get; protected set; }
+    public float Area { get; protected set; }
+    public float ForceToStayAboveGround { get; protected set; }
 
+    public bool AtGround { get; protected set; }
     // Constructor
-    public PhysicsObject(float mass, float initialVelocity, float youngsModulus, float maxDeformation, int groundLevel, float area, float forceToStayAboveGround)
+    public PhysicsObject(float mass, float initialVelocity, float youngsModulus, float maxDeformation, double groundLevel, float area, bool atGround)
     {
         Mass = mass;
         Velocity = initialVelocity;
@@ -43,7 +57,7 @@ public class PhysicsObject
         MaxDeformation = maxDeformation;
         GroundLevel = groundLevel;
         Area = area;
-        ForceToStayAboveGround = forceToStayAboveGround;
+        AtGround = atGround;
     }
 
     // Update method to simulate physics over time
@@ -52,26 +66,27 @@ public class PhysicsObject
         float acceleration = appliedForce / Mass;
         Velocity += acceleration * deltaTime;
 
-        int newY = (int)(GroundLevel + Deformation * Area); // calculate the new Y-coordinate based on the ground level and deformation
-
-
-
-        // check if the object is below the ground level
-        if (newY =< GroundLevel)
-        {
-            float forceToResistGravity = ForceToStayAboveGround - appliedForce;
-            float accelerationToStayAboveGround = forceToResistGravity / Mass;
-            Velocity += accelerationToStayAboveGround * deltaTime;
-        }
+        double newY = (double)(GroundLevel + Deformation * Area); // calculate the new Y-coordinate based on the ground level and deformation
 
         // calculate deformation, this doesnt even work in this situation anymore, i need to adjust so much further
         float stress = appliedForce / Area;
         Deformation = stress / YoungsModulus;
 
+        // check if the object is below the ground level
+        if (newY < GroundLevel) //checks if object is very slightly below ground level
+        {
+            float forceToResistGravity = appliedForce;
+            float accelerationToStayAboveGround = -(forceToResistGravity / Mass);
+            Velocity = 0;
+            stress = (2 * appliedForce) / Area;
+            Deformation = stress / YoungsModulus;
+            AtGround = true;
+        }
+
         Console.WriteLine($"Time: {DateTime.Now}, Applied Force: {appliedForce}, Deformation: {Deformation}, Velocity: {Velocity}");
 
         // Check for failure based on deformation threshold and velocity
-        if (Deformation > MaxDeformation && Velocity > 0)
+        if (Deformation > MaxDeformation || Velocity > 0)
         {
             Console.WriteLine("Object has failed!");
             // then we need to get all the details of the objects failure, where and how and why
@@ -84,38 +99,39 @@ class Program
     static void Main()
     {
         // Creating a list to store 3D coordinates
-        List<Tuple<int, int, int>> coordinatesList = new List<Tuple<int, int, int>>();
+        List<(double x, double y, double z)> coordinatesList = new();
 
         Console.WriteLine("Enter x y z:");
 
-        // Reading input and converting to integers using int.TryParse
-        int x, y, z;
-        if (int.TryParse(Console.ReadLine(), out x) &&
-            int.TryParse(Console.ReadLine(), out y) &&
-            int.TryParse(Console.ReadLine(), out z))
+        // Reading input and converting to doubleegers using double.TryParse
+        double x, y, z;
+        if (double.TryParse(Console.ReadLine(), out x) &&
+            double.TryParse(Console.ReadLine(), out y) &&
+            double.TryParse(Console.ReadLine(), out z))
         {
             AddCoordinates.AddPositiveCoordinates(coordinatesList, x, y, z);
 
             float area = 1.0f; // Assuming the cross-sectional area is 1 unit, adjust based on your object's geometry
-            float forceToStayAboveGround = 10.0f; // Adjust as needed for the force required to stay above the ground
-            PhysicsObject buildingBlock = new PhysicsObject(1.0f, 0.0f, 1.0f, 0.1f, 0, area, forceToStayAboveGround); // Assuming ground level is at Y=0
-
+            PhysicsObject buildingBlock = new PhysicsObject(1.0f, 0.0f, 1.0f, 0.1f, 0, area, false); // Assuming ground level is at Y=0
+            Console.WriteLine("Enter x y z:");
+            double xyz = tuple
             // Simulate physics with an applied force
-            for (int i = 0; i < 10; i++)
+            for (double i = 0; i < 10; i++)
             {
-                buildingBlock.Update(0.1f, 50.0f); // Assuming a small time step of 0.1 seconds and a force of 50 Newtons
+                buildingBlock.Update(0.1f, 50.0f, ); // Assuming a small time step of 0.1 seconds and a force of 50 Newtons
+                                                     // Displaying the coordinates in the list
+                Console.WriteLine("Stored Coordinates:");
+                foreach (var coordinates in coordinatesList)
+                {
+                    Console.WriteLine($"X: {coordinates.x}, Y: {coordinates.y}, Z: {coordinates.z}");
+                }
             }
 
-            // Displaying the coordinates in the list
-            Console.WriteLine("Stored Coordinates:");
-            foreach (var coordinates in coordinatesList)
-            {
-                Console.WriteLine($"X: {coordinates.Item1}, Y: {coordinates.Item2}, Z: {coordinates.Item3}");
-            }
+
         }
         else
         {
-            Console.WriteLine("Invalid input. Please enter valid integers for x, y, and z.");
+            Console.WriteLine("Invalid input. Please enter valid doublegers for x, y, and z.");
         }
     }
 }
---
+++ modelling/modelling/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+
+
+using (_a.Game game = new _a.Game(800, 600, "LearnOpenTK"))
+{
+    game.Run();
+}
+namespace _a
+{
+
+    public class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings(){ ClientSize = (width, height), Title = title }) 
+        {
+        }
+    }
+}
