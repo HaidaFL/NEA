2c447c65cab1c8e37e8f68f6b37ac3a9b8196c76
HaidaFL <152400682+HaidaFL@users.noreply.github.com>
2023-12-11 17:04:46 +0000

    more modelling

--- modelling/modelling/Program.cs
+++ modelling/modelling/Program.cs
@@ -7,15 +7,37 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 using (_a.Game game = new _a.Game(800, 600, "LearnOpenTK"))
 {
+    
     game.Run();
+
 }
 namespace _a
 {
+    //add co-ordinates
 
     public class Game : GameWindow
     {
-        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings(){ ClientSize = (width, height), Title = title }) 
+        public Game(int width, int height, string title) 
+            : base(GameWindowSettings.Default, new NativeWindowSettings()
+            { ClientSize = (width, height), Title = title }) 
         {
+
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.5f, 0.3f, 0.3f, 1.0f);
+
+            //Code goes here
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (KeyboardState.IsAnyKeyDown)
+            {
+                Console.WriteLine("balls");
+            }
         }
     }
 }
